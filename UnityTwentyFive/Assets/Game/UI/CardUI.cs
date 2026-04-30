310be22d915e749e6dd83a220f607ce2ccdfee0b
Eoin Gaughran <eoinbgaughran@gmail.com>
2026-04-30 19:35:36 +0200

    Added Card selection in hand feature

--- UnityTwentyFive/Assets/Game/UI/CardUI.cs
+++ UnityTwentyFive/Assets/Game/UI/CardUI.cs
@@ -1,14 +1,23 @@
 using TMPro;
 using TwentyFiveDotNet.Core.Models;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CardUI : MonoBehaviour
+public class CardUI : MonoBehaviour, IPointerClickHandler
 {
     public TextMeshProUGUI label;
 
-    public void Setup(Card card, bool isHuman)
+    private Card card;
+    private int playerID;
+
+    public System.Action<CardUI, int> OnCardClicked;
+
+    public void Setup(Card card, int playerID)
     {
-        if (isHuman)
+        this.card = card;
+        this.playerID = playerID;
+
+        if (playerID == 0)
         {
             label.text = card.ToString();
         }
@@ -16,5 +25,23 @@ public class CardUI : MonoBehaviour
         {
             label.text = card.ToString();
         }
+
+        Debug.Log(card.ToString() + " cooards: "
+            + "\n x position: " + transform.localPosition.x
+            + "\n y position: " + transform.localPosition.y
+            + "\n z position: " + transform.localPosition.z);
+    }
+
+    public Card GetCard() => card;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnCardClicked?.Invoke(this, playerID);
+    }
+
+    public void SetSelected(bool selected)
+    {
+        float offset = selected ? 30f : 0f;
+        transform.localPosition = new Vector3(transform.localPosition.x, offset, transform.localPosition.z);
     }
 }
--- UnityTwentyFive/Assets/Game/UI/PlayerUI.cs
+++ UnityTwentyFive/Assets/Game/UI/PlayerUI.cs
@@ -6,6 +6,7 @@ public class PlayerUI : MonoBehaviour
 {
     public Transform cardParent;
     public GameObject cardPrefab;
+    public CardUI selectedCard;
 
     public TextMeshProUGUI NameTextUI;
     public TextMeshProUGUI ScoreTextUI;
@@ -38,7 +39,25 @@ public class PlayerUI : MonoBehaviour
             GameObject cardGO = Instantiate(cardPrefab, cardParent);
 
             CardUI cardUI = cardGO.GetComponent<CardUI>();
-            cardUI.Setup(card, player.Id == 0);
+            cardUI.Setup(card, player.Id);
+
+            cardUI.OnCardClicked += HandleCardClicked;
         }
+    }
+
+    void HandleCardClicked(CardUI card, int playerID)
+    {
+        if (playerID != 0) return;
+
+        if(selectedCard != null)
+            selectedCard.SetSelected(false);
+
+        selectedCard = card;
+        selectedCard.SetSelected(true);
+    }
+
+    public Card GetSelectedCard()
+    {
+        return selectedCard?.GetCard();
     }
 }
