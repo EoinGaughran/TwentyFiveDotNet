547b45eb221b7d2d043a5465ef2d30c21153c704
Eoin Gaughran <eoinbgaughran@gmail.com>
2026-05-16 20:38:09 +0200

    ConsoleLogUI now in a scrollable view

--- UnityTwentyFive/Assets/Game/UI/ConsoleLogUI.cs
+++ UnityTwentyFive/Assets/Game/UI/ConsoleLogUI.cs
@@ -1,13 +1,17 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConsoleLogUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI label;
-
+    [SerializeField] private ScrollRect scrollRect;
     public void AppendText(string text)
     {
         label.text += "\n" + text;
+
+        Canvas.ForceUpdateCanvases();
+        scrollRect.verticalNormalizedPosition = 0f;
     }
 
     public void ClearText()
